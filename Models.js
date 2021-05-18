f379b9c4ba313387d856aa5c562338a2e911a6f5
muhmadjaradat <muhmadjradat@gmail.com>
2021-05-18 23:39:09 +0300

    adding lab12

---
+++ Models.js
@@ -0,0 +1,98 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/books', { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+  // we're connected!
+});
+
+const bookSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  status: String,
+
+});
+
+const usersSchema = new mongoose.Schema({
+  email: String,
+  books: [bookSchema],
+});
+
+// const BookModel = mongoose.model('bookModel', bookSchema);
+const UsersModel = mongoose.model('UsersModel', usersSchema);
+console.log(typeof(bookSchema));
+function seedUsers() {
+  const Jaradat = new UsersModel({
+    email :'muhmadjradat@gmail.com',
+    books:[{
+      name: 'The Lord of the Rings',
+      description: 'The dark, fearsome Ringwraiths are searching for a Hobbit.',
+      status: 'Most people have seen the epic movie, but have you read the book?'
+    },
+    {
+      name: 'War and Peace',
+      description:'War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature',
+      status: 'Nominated as one of America’s best-loved novels by PBS’s The Great American Read'
+    },
+    {
+      name: 'Ulysses',
+      description:'This revised volume follows the complete unabridged text as corrected in 1961. Contains the original foreword by the author and the historic court ruling to remove the federal ban. It also contains page references to the first American edition of 1934.',
+      status: 'Joyce’s parallel use of The Odyssey…has the importance of a scientific discovery'
+    },]});
+
+  Jaradat.save();
+}
+
+// seedUsers();
+
+
+
+function callUser (req,res){
+
+  UsersModel.find( function (err, User) {
+    if (err) return console.error(err);
+    console.log(User);
+    res.send(User);
+  });
+
+}
+
+function newBook(req,res){
+  const {name , description, status , email}= req.body;
+
+  UsersModel.find({email:'muhmadjradat@gmail.com'},(err,data)=>{
+    data[0].books.push({
+      name: name,
+      description: description,
+      status: status,
+    });
+    data[0].save();
+    res.send(data[0].books);
+    if (err) {res.send('Error');}
+  });
+}
+
+function deleteBook(req, res) {
+  // TODO: get the index
+  const index = Number(req.params.index);
+  console.log(req.params);
+  // TODO: get the owner name
+  const { email } = req.query;
+  // TODO: find the owner
+  UsersModel.find({ email: email }, (err, data) => {
+    // TODO: filter the cats for the owner and remove the one that matches the index
+
+    const newBookData = data[0].books.filter((book, idx) => {
+      return idx !== index;
+    });
+    data[0].books = newBookData;
+    data[0].save();
+
+    // TODO: respond back to the user with a message that the cat has been deleted
+    res.send(' Cat deleted!');
+  });
+}
+
+module.exports=callUser,newBook,deleteBook;
--- server.js
+++ server.js
@@ -2,72 +2,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const mongoose = require('mongoose');
 require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 app.use(cors());
-
-mongoose.connect('mongodb://localhost:27017/books', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function () {
-  // we're connected!
-});
-
-const bookSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  status: String,
-
-});
-
-const usersSchema = new mongoose.Schema({
-  email: String,
-  books: [bookSchema],
-});
-
-// const BookModel = mongoose.model('bookModel', bookSchema);
-const UsersModel = mongoose.model('UsersModel', usersSchema);
-console.log(typeof(bookSchema));
-function seedUsers() {
-  const Jaradat = new UsersModel({
-    email :'Mohammad Jaradat',
-    books:[{
-      name: 'The Lord of the Rings',
-      description: 'The dark, fearsome Ringwraiths are searching for a Hobbit.',
-      status: 'Most people have seen the epic movie, but have you read the book?'
-    },
-    {
-      name: 'War and Peace',
-      description:'War and Peace broadly focuses on Napoleon’s invasion of Russia in 1812 and follows three of the most well-known characters in literature',
-      status: 'Nominated as one of America’s best-loved novels by PBS’s The Great American Read'
-    },
-    {
-      name: 'Ulysses',
-      description:'This revised volume follows the complete unabridged text as corrected in 1961. Contains the original foreword by the author and the historic court ruling to remove the federal ban. It also contains page references to the first American edition of 1934.',
-      status: 'Joyce’s parallel use of The Odyssey…has the importance of a scientific discovery'
-    },]});
-
-  Jaradat.save();
-}
-
-// seedUsers();
+const callUser=require('./Models');
+const newBook=require('./Models');
+const deleteBook=require('./Models');
 
 
 
-function callUser (req,res){
-
-  UsersModel.find( {email:/Mohammad Jaradat/},'books',function (err, User) {
-    if (err) return console.error(err);
-    console.log(User.email);
-    res.send(User);
-  });
-
-}
 
 app.get('/', homePage);
 app.get('/books', callUser);
+app.post('./books',newBook);
+app.delete('./books',deleteBook);
 
 function homePage(req, res) {
   res.send(' Hi from Express');
